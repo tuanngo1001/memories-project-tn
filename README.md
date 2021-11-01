f8cbd26615bf74a6943caaa1de773e96c810220b
tuanngo1001 <tuan.ngo1001@gmail.com>
2021-11-01 05:13:03 -0500

    inital commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# MERN stack project from JavaScript Mastery Youtube channel
+
+## front-end deployment
+
+https://memories-app-tn.netlify.app/
+
+## back-end deployment
+
+https://memories-project-tn.herokuapp.com/
---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello to Memories-Project-TN API");
+});
+
+const PORT = process.env.PORT || 3003;
+
+mongoose
+  .connect(process.env.CONNECTION_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
